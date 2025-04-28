a17abeb361e120cdd4a148a11e67058beb1164dd
Creeper Eye <iostream1204@gmail.com>
2025-04-28 08:32:22 +0000

    GitHub Actions CI 설정 추가됨.
    - 소스 코드 자동 체크아웃 구성됨.
    - Python 3.13 환경 설정됨.
    - Poetry 설치 및 의존성 관리 추가됨.
    - Pytest를 활용한 자동 테스트 실행됨.

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,31 +1,4 @@
-# import pytest
-# from fastapi.testclient import TestClient
-# import os
-# import sys
-# sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 
-
-# if "pytest" in sys.modules:
-#     from backend.models.plans import Plan
-#     from backend.main import app
-
-#     @pytest.fixture
-#     def client():
-#         return TestClient(app)
-
-#     def test_plan(client):
-
-#         plan = {
-#             "id": 1,
-#             "title": "Test Plan",
-#             "description": "This is a test plan.",
-#             "price": 100.0,
-#             "period": 30,
-#             "created_at": "2023-10-01T00:00:00Z",
-#             "updated_at": "2023-10-01T00:00:00Z"
-#         }
-#         response = client.post("/todos", json=plan)
-#         assert response.status_code == 201
 import pytest
 from fastapi.testclient import TestClient
 import os
@@ -43,5 +16,5 @@ def client():
 
 
 def test_plan(client):
-
+    response = client.get("/plans")
     assert client is not None
